57e7e0ef55f94f3e39321caaa59cd4d5aa48e01f
Tagore555 <tagoreraja555@gmail.com>
2024-08-12 16:25:39 +0530

    first commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler');
 
 const index = asyncHandler(async (req, res) => {
     console.log('Hello World');
+    console.log("hii");
     res.send('Hello World');
 });
 
